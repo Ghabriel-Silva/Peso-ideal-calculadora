31530297106ea5cb8283aed256569e9d382584fd
Gabriel sivla <ghaabriieel@gmail.com>
2023-10-30 22:00:06 -0300

    js  pronto aguardando a estillização
    Terminei os comandos javascript, por enquanto n irei mexer, adicionei classes para começar a estilizar o projeto.

--- js/index.js
+++ js/index.js
@@ -1,82 +1,127 @@
-const form = document.querySelector(".form"); //selciona o meu formulário
-const reload = document.querySelector(".reload");//seleciona o meu botão de reload
+// Seleciona o formulário e o botão de recarregar
+const form = document.querySelector(".form");
+const reload = document.querySelector(".reload");
 
-// função responsavel por evitar o envio das informações, e executar as funções criadas abaixo
-function notsubmit(){ 
-form.addEventListener('submit', function(event){
-event.preventDefault()
+// Função responsável por evitar o envio das informações e executar as funções criadas abaixo
+function notsubmit() {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita o envio do formulário
 
-const peso = parseFloat(document.querySelector('.peso').value)
-const altura = parseFloat(document.querySelector('.altura').value)
+        // Obtém o peso e a altura inseridos pelo usuário
+        const peso = parseFloat(document.querySelector('.peso').value);
+        const altura = parseFloat(document.querySelector('.altura').value);
 
-const  resultado = document.querySelector('.resultado')
+        // Seleciona o elemento de resultado
+        const resultado = document.querySelector('.resultado');
 
-isvalid(peso,altura)
+        // Verifica se os números inseridos são válidos
+        isvalid(peso, altura);
 
-const imc = calculaimc(peso, altura).toFixed(2)
-resultado.innerHTML = `${imc}`;
+        // Calcula o IMC e exibe o resultado
+        const imc = calculaimc(peso, altura).toFixed(2);
+        resultado.innerHTML = `Resultado: ${imc}`;
 
-condicaoresultado(imc)
+        // Mostra a condição com base no IMC
+        condicaoresultado(imc);
 
+        // Mostra as orientações com base no IMC
+        mostraorientacao(imc);
 
-reload.style.display = 'inline-block' // Aqui basicamente estou trazendo o meu botão de reload denovo pois o mesmo estava escondido com display none usando css.
+        // Torna o botão de recarregar visível novamente
+        reload.style.display = 'inline-block';
 
-form.style.display = "none" //basicamente aqui o meu elemento form quando enviado some, isto porque defino o stylo do display como none ou seja, não sera enviado
-})
-};
-
-// função reload carrega a página
-function recarregar(){
-    reload.addEventListener('click', function(){
-        location.reload()
-        })   
-};
-
-recarregar()
-notsubmit()
-
-// função responsavel por calcular o imc, onde tenho 2 parâmetros que tem que ser colocado para retornar o valor do calculo.
-function calculaimc(a,b){
-    return a/(b*2)
-};
-
-// funcão responsavel por verificar se os números são válidos, bassicamente está função verifica se o número é válido caso for ela retorna, se não for um número a função vai  mostrar um erro, função também oculta o resultado do imc e as condições caso n for um número.
-function isvalid(a,b){
-if(!isNaN(a) && !isNaN(b)){
+        // Oculta o formulário
+        form.style.display = "none";
+    });
 }
-else{
-    const condicaoresultado = document.querySelector('.condicao-resultado')
-    condicaoresultado.style.display = "none";
-    
-    const resultado = document.querySelector('.resultado')
-    resultado.style.display = "none";
-    
-    const errorscreen = document.querySelector('.error-screen')
-    errorscreen.innerHTML = `Por favor tente novamente e  digite um números válido`;
+
+// Função para recarregar a página quando o botão de recarregar é clicado
+function recarregar() {
+    reload.addEventListener('click', function() {
+        location.reload();
+    });
 }
-};
-// função responsavel por condicionar e mostrar o resultado, tal resultado vai ser mostrado conforme o  parâmetro adicionado.
-function condicaoresultado(imc){
-   const condicaoresultado = document.querySelector('.condicao-resultado')
-   if (imc < 16) {condicaoresultado.innerHTML = "Muito abaixo do peso";
-   condicaoresultado.classList.add('nivel1')
-   } else if (imc >= 16.1 && imc < 18.4) {
-       condicaoresultado.classList.add('nivel3')
-      condicaoresultado.innerHTML = "Abaixo do peso";
-   } else if (imc >= 18.5 && imc <= 24.9) {
-       condicaoresultado.classList.add('nivelnormal')
-      condicaoresultado.innerHTML = "Peso normal";
-   } else if (imc >= 25 && imc < 29.9) {
-      condicaoresultado.innerHTML = "Sobrepeso";
-      condicaoresultado.classList.add('nivel4')
-   } else if (imc >= 30 && imc < 34.9) {
-      condicaoresultado.innerHTML = "Obesidade grau I.";
-      condicaoresultado.classList.add('nivel5')
-   } else if (imc >= 35 && imc < 39.9) {
-      condicaoresultado.innerHTML = "Obesidade grau II. ";
-      condicaoresultado.classList.add('nivel6')
-   } else {
-      condicaoresultado.innerHTML = "Obesidade grau III";
-      condicaoresultado.classList.add('nivel7')
-   }
-   };
+
+// Chama as funções de recarregar e evitar envio do formulário
+recarregar();
+notsubmit();
+
+// Função responsável por calcular o IMC com base no peso e altura fornecidos
+function calculaimc(a, b) {
+    return a / (b * b);
+}
+
+// Função responsável por verificar se os números inseridos são válidos
+function isvalid(a, b) {
+    if (!isNaN(a) && !isNaN(b)) {
+        // Números válidos
+    } else {
+        // Números inválidos
+
+        // Oculta a condição
+        const condicaoresultado = document.querySelector('.condicao-resultado');
+        condicaoresultado.style.display = "none";
+
+        // Oculta o resultado do IMC
+        const resultado = document.querySelector('.resultado');
+        resultado.style.display = "none";
+
+        // Exibe uma mensagem de erro
+        const errorscreen = document.querySelector('.error-screen');
+        errorscreen.innerHTML = `Por favor, tente novamente e digite números válidos.`;
+    }
+}
+
+// Função responsável por determinar a condição com base no valor do IMC
+function condicaoresultado(imc) {
+    const condicaoresultado = document.querySelector('.condicao-resultado');
+    if (imc < 16) {
+        condicaoresultado.innerHTML = "Muito abaixo do peso";
+        condicaoresultado.classList.add('nivel1');
+    } else if (imc >= 16.1 && imc < 18.4) {
+        condicaoresultado.classList.add('nivel3');
+        condicaoresultado.innerHTML = "Abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        condicaoresultado.classList.add('nivelnormal');
+        condicaoresultado.innerHTML = "Peso normal";
+    } else if (imc >= 25 && imc < 29.9) {
+        condicaoresultado.innerHTML = "Sobrepeso";
+        condicaoresultado.classList.add('nivel4');
+    } else if (imc >= 30 && imc < 34.9) {
+        condicaoresultado.innerHTML = "Obesidade grau I.";
+        condicaoresultado.classList.add('nivel5');
+    } else if (imc >= 35 && imc < 39.9) {
+        condicaoresultado.innerHTML = "Obesidade grau II. ";
+        condicaoresultado.classList.add('nivel6');
+    } else {
+        condicaoresultado.innerHTML = "Obesidade grau III";
+        condicaoresultado.classList.add('nivel7');
+    }
+}
+
+// Função responsável por mostrar as orientações com base no valor do IMC
+function mostraorientacao(imc) {
+    const orientacao = document.querySelector('.orientacao');
+    const orientacaonormal = document.querySelector('.orientacaonormal');
+    const orientacaoexeco = document.querySelector('.orientacaoexeco');
+    const orientacaoexeco1 = document.querySelector('.orientacaoexeco1');
+    const orientacaoexeco2 = document.querySelector('.orientacaoexeco2');
+    const orientacaoexeco3 = document.querySelector('.orientacaoexeco3');
+
+    if (isNaN(imc)) {
+        // Se o IMC não for um número válido, saia da função
+        return;
+    } else if (imc < 18.5) {
+        orientacao.style.display = 'inline-block';
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        orientacaonormal.style.display = 'inline-block';
+    } else if (imc >= 25 && imc <= 29.9) {
+        orientacaoexeco.style.display = 'inline-block';
+    } else if (imc >= 30 && imc <= 34.9) {
+        orientacaoexeco1.style.display = 'inline-block';
+    } else if (imc >= 35 && imc < 40) {
+        orientacaoexeco2.style.display = 'inline-block';
+    } else {
+        orientacaoexeco3.style.display = 'inline-block';
+    }
+}
